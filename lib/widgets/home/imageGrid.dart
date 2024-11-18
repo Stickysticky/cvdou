345c71d63454fac7ed30ffea523366651cc8340e
Olivier Ettlin <olivier.ettlin@3dathome.fr>
2024-11-18 15:41:39 +0100

    Redirection image to website

--- lib/widgets/home/imageGrid.dart
+++ lib/widgets/home/imageGrid.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cvdou/objects/imageResult.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ImageGridWidget extends StatelessWidget {
   final List<ImageResult> images;
@@ -21,7 +22,7 @@ class ImageGridWidget extends StatelessWidget {
         itemCount: images.length,
         itemBuilder: (context, index) {
           return GestureDetector(
-            onTap: () => _showImagePopup(context, images[index].urlImage),
+            onTap: () => _showImagePopup(context, images[index]),
             child: Image.network(
               images[index].urlImage,
               fit: BoxFit.cover,
@@ -42,30 +43,74 @@ class ImageGridWidget extends StatelessWidget {
     );
   }
 
-  void _showImagePopup(BuildContext context, String urlImage) {
+  void _showImagePopup(BuildContext context, ImageResult image) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return Dialog(
-          child: Container(
-            padding: EdgeInsets.all(10),
-            child: Image.network(
-              urlImage,
-              fit: BoxFit.cover,
-              loadingBuilder: (context, child, loadingProgress) {
-                if (loadingProgress == null) {
-                  return child;
-                } else {
-                  return Center(child: CircularProgressIndicator()); // Afficher un indicateur de chargement
-                }
-              },
-              errorBuilder: (context, error, stackTrace) {
-                return Center(child: Icon(Icons.error)); // Afficher une icône d'erreur dans le popup
-              },
-            ),
+          child: Stack(
+            children: [
+              // Image affichée en plein écran
+              Container(
+                padding: EdgeInsets.all(10),
+                child: Image.network(
+                  image.urlImage,
+                  fit: BoxFit.cover,
+                  loadingBuilder: (context, child, loadingProgress) {
+                    if (loadingProgress == null) {
+                      return child;
+                    } else {
+                      return Center(child: CircularProgressIndicator());
+                    }
+                  },
+                  errorBuilder: (context, error, stackTrace) {
+                    return Center(child: Icon(Icons.error));
+                  },
+                ),
+              ),
+              // Icône en haut à gauche pour rediriger vers l'URL
+              Positioned(
+                top: 10,
+                right: 10,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).pop(); // Ferme le popup
+                    _redirectToUrl(context, image.urlSource); // Redirige vers l'URL
+                  },
+                  child: Container(
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: Colors.black.withOpacity(0.6), // Fond semi-transparent
+                      shape: BoxShape.circle,
+                    ),
+                    child: Icon(
+                      Icons.link,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         );
       },
     );
+  }
+
+  void _redirectToUrl(BuildContext context, String url) async {
+    final Uri uri = Uri.parse(url);
+
+    // Vérifiez si l'URL peut être ouverte
+    if (await canLaunchUrl(uri)) {
+      // Ouvre l'URL dans le navigateur
+      await launchUrl(uri, mode: LaunchMode.externalApplication);
+    } else {
+      // Affiche un message d'erreur si l'URL ne peut pas être ouverte
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Impossible d\'ouvrir le lien'),
+        ),
+      );
+    }
   }
 }
